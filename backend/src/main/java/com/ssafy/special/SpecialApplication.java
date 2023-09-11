aa89820b6833b4063bfbdf9f3c9dfb9fd4d05fc5
이대건 <ldg031@naver.com>
2023-09-11 13:37:52 +0900

    Feat/BE : 스프링 프로젝트 생성

---
+++ backend/src/main/java/com/ssafy/special/SpecialApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.special;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialApplication.class, args);
+	}
+
+}
