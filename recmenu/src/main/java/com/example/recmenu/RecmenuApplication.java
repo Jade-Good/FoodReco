3cbbe840e03738ec9c13d30326b2d132c2aa50fb
이대건 <ldg031@naver.com>
2023-09-11 11:21:53 +0900

    Feat/BE : 스프링 프로젝트 생성

---
+++ recmenu/src/main/java/com/example/recmenu/RecmenuApplication.java
@@ -0,0 +1,13 @@
+package com.example.recmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecmenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecmenuApplication.class, args);
+	}
+
+}
