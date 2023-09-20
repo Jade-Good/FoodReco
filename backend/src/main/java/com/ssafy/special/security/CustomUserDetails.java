173c2328790dde28a273c81af9a6f832704c2ce1
jjunehee <jjhjjh1159@gmail.com>
2023-09-20 17:02:57 +0900

    Feat : ãRedi이ës©일 인증

---
+++ backend/src/main/java/com/ssafy/special/security/CustomUserDetails.java
@@ -0,0 +1,57 @@
+package com.ssafy.special.security;
+
+import com.ssafy.special.domain.member.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+    private final Member member;
+
+    public CustomUserDetails(Member member) {
+        this.member = member;
+    }
+
+    public final Member getMember() {
+        return member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collectors = new ArrayList<>();
+        collectors.add(()->{return "ROLE_USER";}); //add에 들어올 파라미터는 GrantedAuthority밖에 없으니
+        return collectors;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
