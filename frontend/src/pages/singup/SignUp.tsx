0a37baf8b2fc207dc09637df80a4cf62a0ca3f15
Jade_Good <kangsm423@gmail.com>
2023-09-24 23:26:53 +0900

    Fix/FE : 이메일 인증 수정

--- frontend/src/pages/singup/SignUp.tsx
+++ frontend/src/pages/singup/SignUp.tsx
@@ -1,30 +1,27 @@
-import React, { useState } from 'react';
-import StyledIdInputIcon from '../../components/inputs/StyledIdInputIcon';
-import StyledPwInputIcon from '../../components/inputs/StyledPwInputIcon';
-import StyledButton from '../../styles/StyledButton';
-import classes from './SignUp.module.css';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { BsSquare } from 'react-icons/bs';
-import { BsFillCheckSquareFill } from 'react-icons/bs';
-import StyledBasicInput from '../../components/inputs/StyledBasicInput';
+import React, { useState } from "react";
+import StyledIdInputIcon from "../../components/inputs/StyledIdInputIcon";
+import StyledPwInputIcon from "../../components/inputs/StyledPwInputIcon";
+import StyledButton from "../../styles/StyledButton";
+import classes from "./SignUp.module.css";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { BsSquare } from "react-icons/bs";
+import { BsFillCheckSquareFill } from "react-icons/bs";
+import StyledBasicInput from "../../components/inputs/StyledBasicInput";
 
-import StyledEmailInput from '../../components/inputs/StyledEmailInput';
-import {
-  InputContainer,
-  StyledInput,
-} from './../../components/inputs/StyledInputs';
-import axios from 'axios';
-import StyledButtonProps from '../../styles/StyledButtonProps';
-import HeaderLogo from '../../components/header/HeaderLogo';
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import styled from 'styled-components';
-import { Agree } from '../../components/option/Agree';
-import BasicStringSelect from '../../components/option/BasicStringSelect';
-import RadioButtonsGroup from '../../components/option/ColorToggleButton';
-import ColorToggleButton from '../../components/option/ColorToggleButton';
+import StyledEmailInput from "../../components/inputs/StyledEmailInput";
+import { InputContainer, StyledInput } from "./../../components/inputs/StyledInputs";
+import axios from "axios";
+import StyledButtonProps from "../../styles/StyledButtonProps";
+import HeaderLogo from "../../components/header/HeaderLogo";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import styled from "styled-components";
+import { Agree } from "../../components/option/Agree";
+import BasicStringSelect from "../../components/option/BasicStringSelect";
+import RadioButtonsGroup from "../../components/option/ColorToggleButton";
+import ColorToggleButton from "../../components/option/ColorToggleButton";
 
 interface IForm {
   email: string;
@@ -51,11 +48,11 @@ export const SignUp = () => {
   // 패스워드 체크확인
   const [checkPassword, setCheckPassword] = useState(0);
 
-  const [code, setCode] = useState(0);
+  const [code, setCode] = useState("0");
   const [agree1, setAgree1] = useState(0);
   const [agree2, setAgree2] = useState(0);
-  const steps = ['약관동의', '회원 정보', '취향 설문'];
-  const ageList = ['10대', '20대', '30대', '40대', '50대', '60대', '70대이상'];
+  const steps = ["약관동의", "회원 정보", "취향 설문"];
+  const ageList = ["10대", "20대", "30대", "40대", "50대", "60대", "70대이상"];
 
   const {
     register,
@@ -65,33 +62,32 @@ export const SignUp = () => {
     getValues,
     watch,
   } = useForm<IForm>({
-    mode: 'onSubmit',
+    mode: "onSubmit",
     defaultValues: {
-      email: '',
-      password: '',
-      nickname: '',
+      email: "",
+      password: "",
+      nickname: "",
       age: 0,
-      sex: '',
+      sex: "",
       height: 0,
       weight: 0,
       activity: 0,
-      passwordconfirm: '',
+      passwordconfirm: "",
     },
   });
   // watch 함수를 사용하여 email 값을 실시간으로 관찰합니다.
-  const watchedEmail = watch('email');
-  const sendEmail = getValues('email');
+  const watchedEmail = watch("email");
+  const sendEmail = getValues("email");
 
   const codeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // setCode(e.target.value)
+    setCode(e.target.value);
+    console.log("왜 출력이 안되지??");
     console.log(e);
-  }
-
+  };
 
   // 회원가입 로직
   const handleSignUp: SubmitHandler<IForm> = (data) => {
-    const { email, password, nickname, age, sex, height, weight, activity } =
-      data;
+    const { email, password, nickname, age, sex, height, weight, activity } = data;
     // axios
     //   .post(`${process.env.REACT_APP_BASE_URL}signup/email`, {
     //     email,
@@ -105,58 +101,64 @@ export const SignUp = () => {
   //이메일 인증 요청
   const handleSendEmail = () => {
     console.log(sendEmail);
-    
-    const formData = new FormData();
-    
-    formData.append("email", sendEmail);
+
+    // const formData = new FormData();
+    // formData.append("email", sendEmail);
+
+    let data = {
+      email: sendEmail,
+    };
 
     if (errors.email) {
-      alert('이메일을 다시 확인해 주십시오');
+      alert("이메일을 다시 확인해 주십시오");
     } else {
       console.log(`${process.env.REACT_APP_BASE_URL}/member/sendVerification`);
+
       axios
-        .post(
-          // `https://j9b102.p.ssafy.io/api/member/sendVerification`,
-          `${process.env.REACT_APP_BASE_URL}/member/sendVerification`,
-          formData
-        )
+        .post(`${process.env.REACT_APP_BASE_URL}/member/sendVerification`, JSON.stringify(data), {
+          headers: { "Content-Type": `application/json` },
+        })
         .then((res) => {
-          alert('인증번호를 전송했습니다.');
+          alert("인증번호를 전송했습니다.");
 
           console.log(res);
         })
         .catch((err) => {
-          alert('이메일 전송오류 입니다. 이메일을 다시 확인해주세요');
-          console.log('이메일 전송 오류:', err);
+          alert("이메일 전송오류 입니다. 이메일을 다시 확인해주세요");
+          console.log("이메일 전송 오류:", err);
         });
     }
   };
 
   //이메일 인증확인
   const handleCheckEmail = () => {
-
-    const formData = new FormData();
-    
-    formData.append("email", sendEmail);
+    // const formData = new FormData();
+    // formData.append("email", sendEmail);
     // formData.append("code", code);
 
+    let data = {
+      email: sendEmail,
+      // code: String(code),
+      code: "06305",
+    };
+
+    console.log(data);
+
     axios
-      .post(
-        // `https://j9b102.p.ssafy.io/api/member/checkVerification`,
-        `${process.env.REACT_APP_BASE_URL}/member/checkVerification`,
-        formData
-      )
+      .post(`${process.env.REACT_APP_BASE_URL}/member/checkVerification`, JSON.stringify(data), {
+        headers: { "Content-Type": `application/json` },
+      })
       .then((res) => {
-        if (res.status === 200) {
-          console.log('인증확인');
-          console.log(res);
+        console.log(res);
+        if (res.status === 202) {
+          console.log("인증확인");
         } else {
-          alert('인증번호가 틀립니다.');
+          alert("인증번호가 틀립니다.");
         }
       })
       .catch((err) => {
-        alert('인증에 실패했습니다. 인증번호를 확인해주세요');
-        console.log('이메일 인증 오류:', err);
+        alert("인증에 실패했습니다. 인증번호를 확인해주세요");
+        console.log("이메일 인증 오류:", err);
       });
   };
 
@@ -167,16 +169,14 @@ export const SignUp = () => {
       <br />
       <br />
       <br />
-      <p style={{ color: '#525252', fontSize: '1.5rem', fontWeight: 'bold' }}>
-        회원가입
-      </p>
+      <p style={{ color: "#525252", fontSize: "1.5rem", fontWeight: "bold" }}>회원가입</p>
       <br />
 
       <form onSubmit={handleSubmit(handleSignUp)}>
         {/* 회원가입 첫번째 페이지 */}
         {progress === 0 && (
           <div className={classes.containerNoHeight}>
-            <Box sx={{ width: '100%' }}>
+            <Box sx={{ width: "100%" }}>
               <Stepper activeStep={0} alternativeLabel>
                 {steps.map((label) => (
                   <Step key={label}>
@@ -189,10 +189,7 @@ export const SignUp = () => {
             <div className={classes.labelContainer}>
               <label htmlFor="">Service 이용약관 동의</label>
               {agree1 ? (
-                <BsFillCheckSquareFill
-                  style={{ color: '#FE9D3A' }}
-                  onClick={() => setAgree1(0)}
-                />
+                <BsFillCheckSquareFill style={{ color: "#FE9D3A" }} onClick={() => setAgree1(0)} />
               ) : (
                 <BsSquare
                   onClick={() => {
@@ -207,10 +204,7 @@ export const SignUp = () => {
             <div className={classes.labelContainer}>
               <label htmlFor="">개인정보 수집 및 이용에 대한 안내</label>
               {agree2 ? (
-                <BsFillCheckSquareFill
-                  style={{ color: '#FE9D3A' }}
-                  onClick={() => setAgree2(0)}
-                />
+                <BsFillCheckSquareFill style={{ color: "#FE9D3A" }} onClick={() => setAgree2(0)} />
               ) : (
                 <BsSquare
                   onClick={() => {
@@ -227,7 +221,7 @@ export const SignUp = () => {
 
         {progress === 1 && (
           <div className={classes.containerNoHeight}>
-            <Box sx={{ width: '100%' }}>
+            <Box sx={{ width: "100%" }}>
               <Stepper activeStep={1} alternativeLabel>
                 {steps.map((label) => (
                   <Step key={label}>
@@ -243,10 +237,7 @@ export const SignUp = () => {
                   이메일 입력
                 </label>
                 {errors.email && (
-                  <small
-                    role="alert"
-                    style={{ color: 'red', fontSize: '10px' }}
-                  >
+                  <small role="alert" style={{ color: "red", fontSize: "10px" }}>
                     {errors.email.message}
                   </small>
                 )}
@@ -260,9 +251,9 @@ export const SignUp = () => {
                   pattern: {
                     value:
                       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                    message: '이메일 형식에 맞지 않습니다.',
+                    message: "이메일 형식에 맞지 않습니다.",
                   },
-                  required: '이메일은 필수 입력입니다.',
+                  required: "이메일은 필수 입력입니다.",
                   onChange(event) {
                     setCheckEmail(0);
                   },
@@ -277,10 +268,7 @@ export const SignUp = () => {
               {/* 인증번호 확인 */}
               {checkEmail ? (
                 <InputContainer>
-                  <StyledInput 
-                  placeholder="인증번호를 입력하세요"
-                  onChange={codeChange}
-                   />
+                  <StyledInput placeholder="인증번호를 입력하세요" onChange={codeChange} />
                   <StyledButtonProps
                     width="4rem"
                     height="1srem"
@@ -315,10 +303,7 @@ export const SignUp = () => {
                     비밀번호 입력
                   </label>
                   {errors.password && (
-                    <small
-                      role="alert"
-                      style={{ color: 'red', fontSize: '10px' }}
-                    >
+                    <small role="alert" style={{ color: "red", fontSize: "10px" }}>
                       {errors.password.message}
                     </small>
                   )}
@@ -333,21 +318,19 @@ export const SignUp = () => {
                 rules={{
                   pattern: {
                     value: /^(?=.*[a-z])(?=.*\d)[a-zA-Z\d]{8,}$/,
-                    message: '영어,숫자,특수문자가 포함되어야합니다..',
+                    message: "영어,숫자,특수문자가 포함되어야합니다..",
                   },
                   minLength: {
                     value: 8,
-                    message: '8자리 이상 비밀번호를 사용하세요.',
+                    message: "8자리 이상 비밀번호를 사용하세요.",
                   },
-                  required: '비밀번호는 필수 입력입니다.',
+                  required: "비밀번호는 필수 입력입니다.",
 
                   onChange(event) {
                     setCheckPassword(0);
                   },
                 }}
-                aria-invalid={
-                  isSubmitted ? (errors.password ? 'true' : 'false') : undefined
-                }
+                aria-invalid={isSubmitted ? (errors.password ? "true" : "false") : undefined}
               />
             </div>
             {/* 비밀번호확인 */}
@@ -356,7 +339,7 @@ export const SignUp = () => {
                 비밀번호 확인
               </label>
               {errors.passwordconfirm && (
-                <small role="alert" style={{ color: 'red', fontSize: '10px' }}>
+                <small role="alert" style={{ color: "red", fontSize: "10px" }}>
                   {errors.passwordconfirm.message}
                 </small>
               )}
@@ -365,13 +348,13 @@ export const SignUp = () => {
               <StyledInput
                 id="passwordconfirm"
                 type="password"
-                {...register('passwordconfirm', {
+                {...register("passwordconfirm", {
                   required: true,
                   validate: {
                     check: (val) => {
-                      const originalPassword = getValues('password');
+                      const originalPassword = getValues("password");
                       if (originalPassword && originalPassword !== val) {
-                        return '비밀번호가 일치하지 않습니다.';
+                        return "비밀번호가 일치하지 않습니다.";
                       } else {
                         setCheckPassword(1);
                       }
@@ -415,7 +398,7 @@ export const SignUp = () => {
                       setProgress((prevProgress) => prevProgress + 1);
                     }
                   } else {
-                    alert('이메일인증과 비밀번호를 확인해주세요!');
+                    alert("이메일인증과 비밀번호를 확인해주세요!");
                   }
                 }}
               >
@@ -426,7 +409,7 @@ export const SignUp = () => {
         )}
         {progress === 2 && (
           <div className={classes.containerNoHeight}>
-            <Box sx={{ width: '100%' }}>
+            <Box sx={{ width: "100%" }}>
               <Stepper activeStep={1} alternativeLabel>
                 {steps.map((label) => (
                   <Step key={label}>
@@ -442,7 +425,7 @@ export const SignUp = () => {
                 닉네임
               </label>
               {errors.nickname && (
-                <small role="alert" style={{ color: 'red', fontSize: '10px' }}>
+                <small role="alert" style={{ color: "red", fontSize: "10px" }}>
                   {errors.nickname.message}
                 </small>
               )}
@@ -455,9 +438,9 @@ export const SignUp = () => {
               rules={{
                 minLength: {
                   value: 8,
-                  message: '8자리이하 닉네임을 사용해주세요.',
+                  message: "8자리이하 닉네임을 사용해주세요.",
                 },
-                required: '닉네임은 필수 입력입니다.',
+                required: "닉네임은 필수 입력입니다.",
               }}
             />
 
@@ -509,7 +492,7 @@ export const SignUp = () => {
                       setProgress((prevProgress) => prevProgress + 1);
                     }
                   } else {
-                    alert('이메일인증과 비밀번호를 확인해주세요!');
+                    alert("이메일인증과 비밀번호를 확인해주세요!");
                   }
                 }}
               >
