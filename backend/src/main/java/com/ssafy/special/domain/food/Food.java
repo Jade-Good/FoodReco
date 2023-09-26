71578f632efd97f41ea11214be370c5636bc007b
jjunehee <jjhjjh1159@gmail.com>
2023-09-26 11:12:30 +0900

    Feat/BE : signup API ãI, Entity 수정

--- backend/src/main/java/com/ssafy/special/domain/food/Food.java
+++ backend/src/main/java/com/ssafy/special/domain/food/Food.java
@@ -29,17 +29,17 @@ public class Food {
 
     // food_cooking_method
     @NotNull
-    @Column(name = "cooking_method", length = 16)
+    @Column(name = "cooking_method", length = 9)
     private String cookingMethod;
 
     // food_type
     @NotNull
-    @Column(name = "type", length = 12)
+    @Column(name = "type", length = 15)
     private String type;
 
     // food_category
     @NotNull
-    @Column(name = "category", length = 12)
+    @Column(name = "category", length = 20)
     private String category;
 
     // food_association
