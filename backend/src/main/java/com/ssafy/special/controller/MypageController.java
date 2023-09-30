416be8fc78c81ca444e6b48e4742122a448d6bf1
jjunehee <jjhjjh1159@gmail.com>
2023-09-30 16:37:34 +0900

    Fix/be : userSignupDto update

--- backend/src/main/java/com/ssafy/special/controller/MypageController.java
+++ backend/src/main/java/com/ssafy/special/controller/MypageController.java
@@ -1,19 +1,25 @@
+
 package com.ssafy.special.controller;
 
+import com.ssafy.special.dto.request.UserTasteDto;
+import com.ssafy.special.dto.request.UserInfoUpdateDto;
 import com.ssafy.special.dto.response.MemberDetailDto;
+import com.ssafy.special.service.food.FoodService;
 import com.ssafy.special.service.member.MemberService;
 import com.ssafy.special.util.SecurityUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/mypage")
 @RequiredArgsConstructor
@@ -23,38 +29,97 @@ public class MypageController {
     private final MemberService memberService;
     private final SecurityUtils securityUtils;
 
-    @GetMapping("/info")
-    public ResponseEntity<Map<String,Object>> getUserInfo() {
+    private final FoodService foodService;
 
-        Map<String,Object> resultMap = new HashMap<>();
+
+    @GetMapping("/info")
+    public ResponseEntity<Map<String, Object>> getUserInfo() {
+
+        Map<String, Object> resultMap = new HashMap<>();
         HttpStatus status = null;
         try {
             MemberDetailDto memberDetailDto = memberService.getUserInfo(securityUtils.getEmail());
 
             if (memberDetailDto != null) {
-                resultMap.put("memberDetailDto",memberDetailDto);
+                resultMap.put("memberDetailDto", memberDetailDto);
                 status = HttpStatus.OK;
             }
         } catch (NullPointerException e) {
             status = HttpStatus.INTERNAL_SERVER_ERROR;
             resultMap.put("message", e.getMessage());
         }
-        return new ResponseEntity<>(resultMap,status);
+        return new ResponseEntity<>(resultMap, status);
     }
 
 
-//    @GetMapping("/list/favorite")
-//
-//    @GetMapping("/list/hate")
-//
-//    @PatchMapping("/list/favorite")
-//
-//    @PatchMapping("/list/hate")
-//
-//    @PatchMapping("/list/info")
-//
-//    @GetMapping("/list/food")
-//
-//    @GetMapping("/list/food/search/{foodName}")
+    @GetMapping("/list/favorite")
+    public ResponseEntity<Map<String, Object>> getUserFavorite() {
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+        String message = null;
+        try {
+            List<UserTasteDto> userFavoriteList = memberService.getUserPreference("jjhjjh1159@gmail.com", 0);
+
+            message = "조회 성공";
+            status = HttpStatus.OK;
+            resultMap.put("userFavoriteList", userFavoriteList);
+        } catch (Exception e) {
+            message = "조회 실패";
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        resultMap.put("message", message);
+
+        return new ResponseEntity<>(resultMap, status);
+    }
+
+    @GetMapping("/list/hate")
+    public ResponseEntity<Map<String, Object>> getUserHate() {
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+        String message = null;
+        try {
+            List<UserTasteDto> userFavoriteList = memberService.getUserPreference("jjhjjh1159@gmail.com", 1);
+
+            message = "조회 성공";
+            status = HttpStatus.OK;
+            resultMap.put("userFavoriteList", userFavoriteList);
+        } catch (Exception e) {
+            message = "조회 실패";
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        resultMap.put("message", message);
+
+        return new ResponseEntity<>(resultMap, status);
+    }
+
+
+    @PatchMapping("/info")
+    public ResponseEntity<Map<String, String>> updateUserInfo(UserInfoUpdateDto userInfoUpdateDto) {
+
+        Map<String, String> resultMap = new HashMap<>();
+        HttpStatus status = null;
+        String message = "";
+        log.info("아라리오~~~~~~");
+        try {
+            foodService.uploadImg("jjhjjh1159@gmail.com", userInfoUpdateDto.getImg());
+
+//            memberService.updateUserInfo(userInfoUpdateDto);
+            status = HttpStatus.OK;
+            message = "수정 완료";
+        } catch (EntityNotFoundException e) {
+            message = e.getMessage();
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        } catch (Exception e) {
+            message = e.getMessage();
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        resultMap.put("message", message);
+
+        return new ResponseEntity<>(resultMap, status);
+    }
 
 }
+	
