cba84cbaeaab1626e17ef75b715aec7a456938c0
Jade_Good <kangsm423@gmail.com>
2023-10-02 21:55:24 +0900

    Feat/FE : kakao map api

--- frontend/src/components/recommend/foodDetail.tsx
+++ frontend/src/components/recommend/foodDetail.tsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import Modal from "react-modal";
 import { useRecoilState } from "recoil";
 import { foodDetailModal } from "../../recoil/atoms/modalState";
-import { Container as MapDiv, NaverMap, Marker } from "react-naver-maps";
+
+import { Map } from "react-kakao-maps-sdk";
 
 /*overlay는 모달 창 바깥 부분을 처리하는 부분이고,
 content는 모달 창부분이라고 생각하면 쉬울 것이다*/
@@ -17,14 +20,14 @@ const customModalStyles: ReactModal.Styles = {
     left: "0",
   },
   content: {
-    width: "70vw",
-    height: "70vh",
+    width: "80vw",
+    height: "80vh",
     // zIndex: "150",
     position: "absolute",
     top: "50%",
     left: "50%",
 
-    padding: "1rem",
+    padding: "0",
 
     transform: "translate(-50%, -50%)",
     borderRadius: "1rem",
@@ -40,13 +43,46 @@ interface Coords {
   longitude: number | null;
 }
 
+interface Result {
+  address_name?: string;
+  category_group_code?: string;
+  category_group_name?: string;
+  category_name?: string;
+  distance?: string;
+  id?: string;
+  phone?: string;
+  place_name?: string;
+  place_url?: string;
+  road_address_name?: string;
+  x?: string;
+  y?: string;
+}
+
 const FoodDetail = () => {
   const [modal, setModalOpen] = useRecoilState(foodDetailModal);
 
   let foodName = "짜장면";
+  const ps = new kakao.maps.services.Places();
+  // 키워드로 장소를 검색합니다
+  // searchPlaces();
+
+  // 장소검색이 완료됐을 때 호출되는 콜백함수
+  function placesSearchCB(data: any, status: any, pagination: any) {
+    if (status === kakao.maps.services.Status.OK) {
+      console.log(data);
+      setStores(data);
+    } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+      console.log("검색 결과가 존재하지 않습니다.");
+      return;
+    } else if (status === kakao.maps.services.Status.ERROR) {
+      console.log("검색 결과 중 오류가 발생했습니다.");
+      return;
+    }
+  }
 
   const [coords, setCoords] = useState<Coords>({ latitude: null, longitude: null });
   const [error, setError] = useState<string | null>(null);
+  const [stores, setStores] = useState<Result[] | null>(null);
 
   useEffect(() => {
     // 위치 정보 가져오기 시도
@@ -63,7 +99,18 @@ const FoodDetail = () => {
     } else {
       setError("브라우저에서 Geolocation을 지원하지 않습니다.");
     }
+    search();
   }, []);
+
+  const search = () => {
+    console.log("짜장면집 찾아보자");
+    ps.keywordSearch(foodName, placesSearchCB, {
+      category_group_code: "FD6",
+      x: Number(`${coords.longitude}`),
+      y: Number(`${coords.latitude}`),
+      radius: 1000,
+    });
+  };
 
   return (
     <Modal
@@ -74,7 +121,24 @@ const FoodDetail = () => {
       contentLabel="Pop up Message"
       shouldCloseOnOverlayClick={true}
     >
-      <h4 style={{ margin: "0" }}>주변 '{foodName}' 가게</h4>
+      <h4 style={{ margin: "1rem 0rem 1rem 1rem" }}>주변 '{foodName}' 가게</h4>
+
+      <button onClick={search}> 짜장면집 검색</button>
+
+      <Map // 지도를 표시할 Container
+        id="map"
+        center={{
+          // 지도의 중심좌표
+          lat: Number(`${coords.latitude}`),
+          lng: Number(`${coords.longitude}`),
+        }}
+        style={{
+          // 지도의 크기
+          width: "80vw",
+          height: "20vh",
+        }}
+        level={7} // 지도의 확대 레벨
+      />
 
       <div>
         {coords.latitude !== null && coords.longitude !== null ? (
@@ -90,18 +154,47 @@ const FoodDetail = () => {
         )}
       </div>
 
-      <MapDiv
-        style={{
-          height: 400,
-        }}
-      >
-        <NaverMap>
-          <Marker defaultPosition={{ lat: 37.5666103, lng: 126.9783882 }} />
-        </NaverMap>
-      </MapDiv>
+      <hr />
+
+      {stores
+        ? stores.map((result, idx) => {
+            return (
+              <>
+                <ul>
+                  <li>주소이름 : {result.address_name}</li>
+                  <li>그룹코드 : {result.category_group_code}</li>
+                  <li>그룹이름 : {result.category_group_name}</li>
+                  <li>카테고리 : {result.category_name}</li>
+                  <li>거리 : {result.distance}</li>
+                  <li>아이디 : {result.id}</li>
+                  <li>전화번호 : {result.phone}</li>
+                  <li>장소명 : {result.place_name}</li>
+                  <li>url : {result.place_url}</li>
+                  <li>주소 : {result.road_address_name}</li>
+                  <li>x경도 : {result.x}</li>
+                  <li>y위도 : {result.y}</li>
+                </ul>
+                <hr />
+              </>
+            );
+          })
+        : null}
     </Modal>
   );
 };
+
+// address_name?: string;
+//   category_group_code?: string;
+//   category_group_name?: string;
+//   category_name?: string;
+//   distance?: string;
+//   id?: string;
+//   phone?: string;
+//   place_name?: string;
+//   place_url?: string;
+//   road_address_name?: string;
+//   x?: string;
+//   y?: string;
 
 export default FoodDetail;
 
