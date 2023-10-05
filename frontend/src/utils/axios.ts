a9ebe0bbfc212c58fe22fe3396b7aba277eed9e6
kimdongwoo <siwol406@gmail.com>
2023-10-05 22:28:20 +0900

    Docs/FE : env환경변수설정

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -109,9 +109,7 @@ api.interceptors.response.use(
                 progress: undefined,
                 theme: "colored",
               });
-              // window.location.replace(
-              //   `${process.env.REACT_APP_BASE_URL}/login`
-              // );
+              window.location.replace(`https://j9b102.p.ssafy.io/login`);
             }
             return;
           });
@@ -131,7 +129,7 @@ api.interceptors.response.use(
         progress: undefined,
         theme: "colored",
       });
-      // window.location.replace(`${process.env.REACT_APP_BASE_URL}/login`);
+      window.location.replace(`https://j9b102.p.ssafy.io/login`);
     }
 
     return Promise.reject(error);
